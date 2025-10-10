01d6f8c22fdd8a29353e8404c133525b7d8a4452
GSuma21 <21gsuma@gmail.com>
2025-10-10 16:52:53 +0530

    fix for dashboard center in mobile screen , legends position change for page wise

--- src/app/pages/country-view/country-view.ts
+++ src/app/pages/country-view/country-view.ts
@@ -29,6 +29,7 @@ export class CountryView implements OnInit, AfterViewInit {
   @Input() notes: any = [];
   selectedIndicator: string = 'Micro Improvements Initiated';
   hoveredState: string = "";
+  dashboardPage = window.location.pathname.includes('/dashboard') ? true : false
 
   indicatorData: { value: number | string; label: string }[] = [];
   baseUrl: any = `${environment.storageURL}/${environment.bucketName}/${environment.folderName}`;
