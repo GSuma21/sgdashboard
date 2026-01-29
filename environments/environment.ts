6b055dfce9bc6bf3a525c5e1b296361f12494531
Syed Ameen <syedameenabrar786@gmail.com>
2026-01-29 11:50:09 +0530

    share navtive block in windowns

--- environments/environment.ts
+++ environments/environment.ts
@@ -20,5 +20,5 @@ export const environment = {
     whatsapp: 'https://web.whatsapp.com/send?',
     facebook: 'https://www.facebook.com/sharer/sharer.php?u=',
     instagram: 'https://www.instagram.com',
-    shareText : 'This story really stayed with me, and I wanted to share it with you. Do take a moment to read it and help amplify these voices from the ground.'
+    shareText : 'This story really stayed with me, and I wanted to share it with you. Do take a moment to read it and help amplify these voices from the ground.\n'
 };
