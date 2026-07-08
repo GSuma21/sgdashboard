16cf5672f88e477ca359e3c34b7484ac1fe44735
GSuma21 <21gsuma@gamil.com>
2026-07-08 22:58:19 +0530

    Story 1914(footer changes) , Story 1549 (line chart changes)

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,3 +1,28 @@
+// export const environment = {
+//     production: true,
+//     storageURL: 'https://storage.googleapis.com',
+//     bucketName: 'prod-sg-dashbaord',
+//     folderName: 'sg-dashboard',
+//     firebasePaths: {
+//         root: 'sg-dashboard',
+//         subCollection: 'browsers'
+//     },
+//     firebaseConfig: {
+//         apiKey: "AIzaSyACIsfJ3ibaqxcPEhxGQVqV9AW-cBnLqCg",
+//         authDomain: "sl-prod-d5850.firebaseapp.com",
+//         projectId: "sl-prod-d5850",
+//         storageBucket: "sl-prod-d5850.firebasestorage.app",
+//         messagingSenderId: "249304500920",
+//         appId: "1:249304500920:web:6823104ce87ecaf592c24d"
+//     },
+//     shareLink: 'https://dashboard.shikshagraha.org/',
+//     linkedin: 'https://www.linkedin.com/sharing/share-offsite//?url=',
+//     whatsapp: 'https://web.whatsapp.com/send?',
+//     facebook: 'https://www.facebook.com/sharer/sharer.php?u=',
+//     instagram: 'https://www.instagram.com',
+//     shareText: 'This story really stayed with me, and I wanted to share it with you. Do take a moment to read it and help amplify these voices from the ground.\n'
+// };
+
 export const environment = {
     production: true,
     storageURL:'https://storage.googleapis.com',
